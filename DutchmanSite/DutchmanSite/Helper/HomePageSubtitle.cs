c7cd224743483953e1e12218f2b9285d77ca3f98
JeremyT <waddledoocanto@gmail.com>
2018-06-30 16:33:18 -0700

    Added subtitles

--- DutchmanSite/DutchmanSite/Helper/HomePageSubtitle.cs
+++ DutchmanSite/DutchmanSite/Helper/HomePageSubtitle.cs
@@ -11,16 +11,27 @@ namespace DutchmanSite.Helper
                 "Whisper your secrets to us",
                 "We're Trapped!",
                 "From a lost city, hundreds of miles below the Superstition Mountains",
-                "I'm supposted to use this line to convince you to buy our products",
+                "I'm supposed to use this line to convince you to buy our products",
                 "We'll argue about anything",
                 "Live from an isolated gulch under a full moon!",
                 "Doing....Something..",
-                "We dont even know what we do",
-                "A form of bussiness that consists of buzzwords, esoteric code, and incomprehensable design",
+                "We don't even know what we do",
+                "A form of business that consists of buzzwords, esoteric code, and incomprehensible design",
                 "Powered by crystals!",
                 "The interpolation mode also joins or breaks left and right tangents",
-                "We're winning the Arms Race",
-                "Lorem ipsum dolor sit amet"
+                "'Winning' the Arms Race",
+                "Lorem ipsum dolor sit amet",
+                "I like your shirt",
+                "Dry-roasted flavorless wild caught RPG's",
+                "Precipitation: 0%",
+                "https://libraryofbabel.info/bookmark.cgi?found_it:5",
+                "The voice of a cat being pet”",
+                "I <3 C# Lists",
+                "Don't let me forget to pick up some Jalapenos on the way home",
+                "It's pronounced Mango!",
+                "The good stuffs further down",
+                "We won't tell"
+
         };
 
         public string GetSubtitle()
