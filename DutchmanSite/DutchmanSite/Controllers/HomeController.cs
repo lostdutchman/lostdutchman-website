f4ab368b6bc534e2aa52b57d06be31ea6e6c92d3
Jeremy Toler <waddledoocanto@gmail.com>
2019-03-14 13:02:11 -0700

    Removed Dev Logs
    Added Nice Bowling Privacy Policy
    Removed Logs
    Removed all database everything

--- DutchmanSite/DutchmanSite/Controllers/HomeController.cs
+++ DutchmanSite/DutchmanSite/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using DutchmanSite.Models;
-using DutchmanSite.Helper;
+﻿using DutchmanSite.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,35 +7,6 @@ using System.Web.Mvc;
 
 namespace DutchmanSite.Controllers
 {
-    public class BasicAuthenticationAttribute : ActionFilterAttribute
-    {
-        public string BasicRealm { get; set; }
-        protected string Username { get; set; }
-        protected string Password { get; set; }
-
-        public BasicAuthenticationAttribute(string username, string password)
-        {
-            this.Username = "Be careful what you wish for.";
-            this.Password = "It probably won't come true and life is mostly about expectation management.";
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            var req = filterContext.HttpContext.Request;
-            var auth = req.Headers["Authorization"];
-            if (!String.IsNullOrEmpty(auth))
-            {
-                var cred = System.Text.ASCIIEncoding.ASCII.GetString(Convert.FromBase64String(auth.Substring(6))).Split(':');
-                var user = new { Name = cred[0], Pass = cred[1] };
-                if (user.Name == Username && user.Pass == Password) return;
-            }
-            var res = filterContext.HttpContext.Response;
-            res.StatusCode = 401;
-            res.AddHeader("WWW-Authenticate", String.Format("Basic realm=\"{0}\"", BasicRealm ?? "LostDutchamnSoftware"));
-            res.End();
-        }
-    }
-
     public class HomeController : Controller
     {
         [HttpGet]
@@ -89,59 +59,9 @@ namespace DutchmanSite.Controllers
         }
 
         [HttpGet]
-        public ActionResult BetaTest()
-        {
-            return Redirect("https://goo.gl/forms/zYEzG8BRxkw0F3Ho2");
-        }
-
-        [HttpGet]
-        [BasicAuthenticationAttribute("your-username", "your-password")]
-        public ActionResult DevLogs()
+        public ActionResult NiceBowlingLegalJargon()
         {
             return View();
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        [BasicAuthenticationAttribute("your-username", "your-password")]
-        public ActionResult DevLogs(string site, string icon, string iconText, string title, string summery, string description, int isPublished)
-        {
-            DevLogsModel DevLog = new DevLogsModel();
-            DevLog.CreateNew(site, icon, iconText, title, summery, description, isPublished);
-            return RedirectToAction("DevLogs");
-        }
-
-        [HttpGet]
-        [BasicAuthenticationAttribute("your-username", "your-password")]
-        public ActionResult EditDevLog()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        [BasicAuthenticationAttribute("your-username", "your-password")]
-        public ActionResult EditDevLog(string site, string icon, string iconText, string title, string summery, string description, int isPublished, long logID)
-        {
-            DevLogsModel DevLog = new DevLogsModel();
-            if(isPublished == 2)
-            {
-                DevLog.DeleteLog(logID);
-            }
-            else
-            {
-                DevLog.CreateNew(site, icon, iconText, title, summery, description, isPublished);
-                DevLog.DeleteLog(logID);
-            }
-            return RedirectToAction("DevLogs");
-        }
-
-        [HttpPost]
-        [BasicAuthenticationAttribute("your-username", "your-password")]
-        public ActionResult UploadImage(HttpPostedFileBase file)
-        {
-            ImgUpload ImageHelp = new ImgUpload();
-            return Json(ImageHelp.Upload(file));
         }
     }
 }
