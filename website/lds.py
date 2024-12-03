b4cd35da39da27cae5d2548a9d73e532476bd85b
Jeremy Toler <waddledoocanto@gmail.com>
2024-12-03 14:06:41 -0700

    turning off debug mode

--- website/lds.py
+++ website/lds.py
@@ -29,7 +29,7 @@ def internal_server_error(e):
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-	app.run(debug=True)
-    #app.run(host='0.0.0.0')
+	#app.run(debug=True)
+    app.run(host='0.0.0.0')
 
 
